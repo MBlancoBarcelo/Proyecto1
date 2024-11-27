1f3f0335dce15cb2bd7fb6dbaa51f43d9ef42ee6
MBlancoBarcelo <mblancobarcelo@gmail.com>
2024-11-27 20:45:36 +0100

    cambios 27/11/2024

--- src/Fractions.java
+++ src/Fractions.java
@@ -15,9 +15,44 @@ public class Fractions {
         String[] partes = s.split("/");
         int numerador = Integer.parseInt(partes[0]);
         int denomirador = Integer.parseInt(partes[1]);
-        String N = leerNumerador(numerador);
+
+
+        String N = numeradorHasta100(numerador);
         String D = leerDenominador(denomirador, numerador);
-        return N + " " + D;
+
+        if (numerador == denomirador) {
+            return "Un";
+        }
+        if (pasarAUnidadesNumerador(numerador) > 19) {
+            String N1 = leerNumeradorHastaEl19(numerador);
+        }
+
+        // Juntar numberador y denominador y pasar a mayuscula
+        String resultado = N + " " + D;
+        resultado = pasarAMayusculaPimeraPalabra(resultado);
+        return resultado;
+    }
+
+    public static String pasarAMayusculaPimeraPalabra(String resultado) {
+        String cambio = resultado.substring(0,1).toUpperCase() + resultado.substring(1) ;
+        return cambio;
+    }
+
+    public  static String numeradorHasta100(int numerador) {
+        if (numerador < 20) return leerNumeradorHastaEl19(numerador);
+        int decimales = numerador / 100;
+        int unidades = numerador % 100;
+        if (decimales == 2) {
+            return "vint-i-" + leerNumeradorHastaEl19(unidades);
+        } else if (decimales == 3) {
+            return "trenta-i-" + leerNumeradorHastaEl19(unidades);
+        } else if (decimales == 4) {
+            return "quaranta-" + leerNumeradorHastaEl19(unidades);
+        } else if (decimales == 5) {
+            return "cinquanta-" + leerNumeradorHastaEl19(unidades);
+        } else if (decimales == 6) {
+
+        }
     }
 
 
@@ -30,20 +65,13 @@ public class Fractions {
     //cuando el numerador sea mayor a 1 seran en singular en el resto de casos sera prural y las potencian
     // de 10 son èsim
 
-    public static int dividirNumeros (int numerador,int denominador) {
-        int unidades;
-        int decimas = 0;
-        while (numerador >= 10) {
-            numerador = numerador / 10;
-            decimas++;
-
-        }
-        unidades = numerador % denominador;
+    public static int pasarAUnidadesNumerador (int numerador) {
+        int unidades = numerador % 10;
         return unidades;
+
     }
 
-
-    public static String leerNumerador(int numerador) {
+    public static String leerNumeradorHastaEl19(int numerador) {
         if (numerador == 1) {
             return "un";
         }
@@ -163,61 +191,61 @@ public class Fractions {
         }
         if (denominador == 10) {
             if (numerador == 1) {
-                return "décim";
+                return "dècim";
             }
-            return "décims";
+            return "dècims";
         }
         if (denominador == 11) {
             if (numerador == 1) {
-                return "onzé";
+                return "onzè";
             }
             return "onzens";
         }
         if (denominador == 12) {
             if (numerador == 1) {
-                return "dotzé";
+                return "dotzè";
             }
             return "dotzens";
         }
         if (denominador == 13) {
             if (numerador == 1) {
-                return "tretzé";
+                return "tretzè";
             }
             return "tretzens";
         }
         if (denominador == 14) {
             if (numerador == 1) {
-                return "catorzé";
+                return "catorzè";
             }
             return "catorzens";
         }
         if (denominador == 15) {
             if (numerador == 1) {
-                return "quienzé";
+                return "quienzè";
             }
             return "quinzens";
         }
         if (denominador == 16) {
             if (numerador == 1) {
-                return "setzé";
+                return "setzè";
             }
             return "setzens";
         }
         if (denominador == 17) {
             if (numerador == 1) {
-                return "dissete";
+                return "dissetè";
             }
             return "dissetens";
         }
         if (denominador == 18) {
             if (numerador == 1) {
-                return "divuité";
+                return "divuitè";
             }
             return "divuitens";
         }
         if (denominador == 19) {
             if (numerador == 1) {
-                return "dinové";
+                return "dinovè";
             }
             return "dinovens";
         }
